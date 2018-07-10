83db8c921ad79b0c0f620175542a684af8c65297
Bruno Lemos <brunolemos@users.noreply.github.com>
2018-07-10 03:07:06 -0300

    Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+declare module 'redux-persist-filesystem-storage' {
+  import RNFetchBlob from 'react-native-fetch-blob'
+
+  interface Options {
+    storagePath: string
+    encoding: string
+    toFileName: (name: string) => string
+    fromFileName: (name: string) => string
+  }
+
+  const FilesystemStorage: {
+    config: (customOptions: Partial<Options>) => Options
+
+    setItem: (
+      key: string,
+      value: string,
+      callback?: (error?: Error) => void,
+    ) => Promise<void>
+
+    getItem: (
+      key: string,
+      callback: (error?: Error, result?: string) => void,
+    ) => ReturnType<RNFetchBlob.fs.readFile> | Promise<undefined>
+
+    removeItem: (
+      key: string,
+      callback: (error?: Error) => void,
+    ) => Promise<undefined>
+
+    getAllKeys: (
+      callback: (error?: Error, keys?: Array<string>) => void,
+    ) => Promise<string[]> | Promise<undefined>
+
+    clear: (callback: (error?: Error) => void) => Promise<boolean>
+  }
+
+  export default FilesystemStorage
+}
